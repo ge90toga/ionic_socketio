a7f8d386b75337cfcd7763b45a220f9b720bf3c1
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-11 16:18:31 +1100

    Add a new friend list tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,83 +2,83 @@ angular.module('chat.controllers', [])
 
   .controller('ChatCtrl', function ($scope, $stateParams, $ionicPopup, $timeout, Socket, Chat,$log) {
 
-    $scope.data = {};
-    $scope.data.message = "";
-    $scope.messages = Chat.getMessages();
-    var typing = false;
-    var lastTypingTime;
-    var TYPING_TIMER_LENGTH = 250;
-
-    Socket.on('connect', function () {
-
-      if (!$scope.data.username) {
-        var nicknamePopup = $ionicPopup.show({
-          template: '<input id="usr-input" type="text" ng-model="data.username" autofocus>',
-          title: 'What\'s your nickname?',
-          scope: $scope,
-          buttons: [{
-            text: '<b>Save</b>',
-            type: 'button-positive',
-            onTap: function (e) {
-              if (!$scope.data.username) {
-                e.preventDefault();
-              } else {
-                return $scope.data.username;
-              }
-            }
-          }]
-        });
-        nicknamePopup.then(function (username) {
-          Socket.emit('add user', username);
-          Chat.setUsername(username);
-        });
-      }
-
-    });
-
-    Chat.scrollBottom();
-
-    if ($stateParams.username) {
-      $scope.data.message = "@" + $stateParams.username;
-      document.getElementById("msg-input").focus();
-    }
-
-    var sendUpdateTyping = function () {
-      if (!typing) {
-        typing = true;
-        Socket.emit('typing');
-      }
-      lastTypingTime = (new Date()).getTime();
-      $timeout(function () {
-        var typingTimer = (new Date()).getTime();
-        var timeDiff = typingTimer - lastTypingTime;
-        if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
-          Socket.emit('stop typing');
-          typing = false;
-        }
-      }, TYPING_TIMER_LENGTH);
-    };
-
-    $scope.updateTyping = function () {
-      sendUpdateTyping();
-    };
-
-    $scope.messageIsMine = function (username) {
-      return $scope.data.username === username;
-    };
-
-    $scope.getBubbleClass = function (username) {
-      var classname = 'from-them';
-      if ($scope.messageIsMine(username)) {
-        classname = 'from-me';
-      }
-      return classname;
-    };
-
-    $scope.sendMessage = function (msg) {
-      Chat.sendMessage(msg);
-      $scope.data.message = "";
-    };
+    // $scope.data = {};
+    // $scope.data.message = "";
+    // $scope.messages = Chat.getMessages();
+    // var typing = false;
+    // var lastTypingTime;
+    // var TYPING_TIMER_LENGTH = 250;
+    //
+    // Socket.on('connect', function () {
+    //
+    //   if (!$scope.data.username) {
+    //     var nicknamePopup = $ionicPopup.show({
+    //       template: '<input id="usr-input" type="text" ng-model="data.username" autofocus>',
+    //       title: 'What\'s your nickname?',
+    //       scope: $scope,
+    //       buttons: [{
+    //         text: '<b>Save</b>',
+    //         type: 'button-positive',
+    //         onTap: function (e) {
+    //           if (!$scope.data.username) {
+    //             e.preventDefault();
+    //           } else {
+    //             return $scope.data.username;
+    //           }
+    //         }
+    //       }]
+    //     });
+    //     nicknamePopup.then(function (username) {
+    //       Socket.emit('add user', username);
+    //       Chat.setUsername(username);
+    //     });
+    //   }
+    //
+    // });
+    //
+    // Chat.scrollBottom();
+    //
+    // if ($stateParams.username) {
+    //   $scope.data.message = "@" + $stateParams.username;
+    //   document.getElementById("msg-input").focus();
+    // }
+    //
+    // var sendUpdateTyping = function () {
+    //   if (!typing) {
+    //     typing = true;
+    //     Socket.emit('typing');
+    //   }
+    //   lastTypingTime = (new Date()).getTime();
+    //   $timeout(function () {
+    //     var typingTimer = (new Date()).getTime();
+    //     var timeDiff = typingTimer - lastTypingTime;
+    //     if (timeDiff >= TYPING_TIMER_LENGTH && typing) {
+    //       Socket.emit('stop typing');
+    //       typing = false;
+    //     }
+    //   }, TYPING_TIMER_LENGTH);
+    // };
+    //
+    // $scope.updateTyping = function () {
+    //   sendUpdateTyping();
+    // };
+    //
+    // $scope.messageIsMine = function (username) {
+    //   return $scope.data.username === username;
+    // };
+    //
+    // $scope.getBubbleClass = function (username) {
+    //   var classname = 'from-them';
+    //   if ($scope.messageIsMine(username)) {
+    //     classname = 'from-me';
+    //   }
+    //   return classname;
+    // };
+    //
+    // $scope.sendMessage = function (msg) {
+    //   Chat.sendMessage(msg);
+    //   $scope.data.message = "";
+    // };
 
   })
 
@@ -90,12 +90,11 @@ angular.module('chat.controllers', [])
     $scope.chat = Chats.get($stateParams.chatId);
   })
 
-  .controller('ChatListCtrl', function ($scope, $log) {
-    $log.info('ChatList');
+  .controller('ChatListCtrl', function ($scope) {
     $scope.chats = [ // some fake data
       {name:'Mike', lastText:'How are you?', face:'/img/max.png'},
       {name:'Linus', lastText:'Thanks',face:'/img/perry.png'},
-      {name:'iHorde', lastText:'Meet me after 5.',face:'img/adam.jpg'},
+      {name:'iHorde', lastText:'Meet me after 5.',face:'img/adam.jpg'}
     ];
   })
 
